c423bb7a7fdaad9989e77904df2e68f3bc74cef6
James Hallberg <Grebllah81788@gmail.com>
2023-07-22 15:32:55 -0400

    complete palindrome lab

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const letters = word.split("")
+  const reversedWordArr = []
+  for (const letter of letters) {
+    reversedWordArr.unshift(letter)
+  }
+  const reversedWord = reversedWordArr.join("")
+  if (reversedWord === word) {
+    return true
+  }
+  else {return false}
 }
 
 /* 
-  Add your pseudocode here
+  1. take the string, turn it into an array, reverse it, stringify it
+  2. store the reversed word as a var
+  3. compare word and reversedWord
+  4. if equal, return true, otherwise false
 */
 
 /*
-  Add written explanation of your solution here
+  First split the word into letters.
+  Then take each letter left to right and add it to the beginning of a new array, reversing the letters.
+  Then rejoin the reversed letters to create a reversed word and save it to a variable.
+  Then return true if reversed word and input word is the same, false if not.
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +36,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("sandwich"))
 }
 
 module.exports = isPalindrome;
